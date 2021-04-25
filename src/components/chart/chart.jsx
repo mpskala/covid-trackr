7c645fef90700d973ff6ba601a8eff2e398eec15
Matt Skala <mattskala2@gmail.com>
2021-04-25 22:55:28 +0100

    updated chart

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Line, Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 import { fetchDailyData } from '../../api';
 
@@ -18,26 +18,26 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     fetchMyAPI();
   }, []);
 
-  const barChart = (
-    confirmed ? (
-      <Bar
-        data={{
-          labels: ['Infected', 'Recovered', 'Deaths'],
-          datasets: [
-            {
-              label: 'People',
-              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
-              data: [confirmed.value, recovered.value, deaths.value],
-            },
-          ],
-        }}
-        options={{
-          legend: { display: false },
-          title: { display: true, text: `Current state in ${country}` },
-        }}
-      />
-    ) : null
-  );
+  // const barChart = (
+  //   confirmed ? (
+  //     <Bar
+  //       data={{
+  //         labels: ['Infected', 'Recovered', 'Deaths'],
+  //         datasets: [
+  //           {
+  //             label: 'People',
+  //             backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+  //             data: [confirmed.value, recovered.value, deaths.value],
+  //           },
+  //         ],
+  //       }}
+  //       options={{
+  //         legend: { display: false },
+  //         title: { display: true, text: `Current state in ${country}` },
+  //       }}
+  //     />
+  //   ) : null
+  // );
 
   const lineChart = (
     dailyData[0] ? (
